e4621d75221fd8721cfc10193a1bfbc00128d092
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-01-03 10:56:03 +0900

    사원등록 프로그램 V1 - 사원정보 조회

--- bamyam/empv1.py
+++ bamyam/empv1.py
@@ -4,7 +4,17 @@ emps = []
 def load_employees():
     print('프로그램을 초기화합니다')
     print('프로그램이 성공적으로 초기화 되었습니다')
+    global emps
+    dicts = []
 
+    with open('employee,csv') as f:
+        datas = f.readlines()
+
+    for data in datas:
+        item = data.strip().split(',')
+        d = {'empno': item[0], 'fname': item[1], 'lname': item[2], 'email': item[3],
+             'hdate':item[4], 'jobid':item[5],'sal':item[6], 'deptid':item[7]}
+        dicts.append(d)
 
 def show_menu():
     main_menu = '''
@@ -41,6 +51,7 @@ def save_employee(emp):
           f"{emp['email']},{emp['hdate']},{emp['jobid']},{emp['sal']},{emp['deptid']}\n")
     with open('employee.csv', 'a', encoding='utf-8') as f:
         f.write(row)
+    emps.append(row)
 
 def add_employee():
     print('사원정보를 등록합니다')
@@ -49,9 +60,13 @@ def add_employee():
 
 
 
-def read_employee():
+def read_employee(emps):
     print('모든 사원정보를 조회합니다')
-
+    result = ''
+    for emp in emps:
+        result += (f"{emp['empno']}\t{emp['fname']}\t"
+                   f"{emp['jobid']}\t{emp['deptid']}\n")
+    print(result)
 
 def readone_employee():
     print('특정 사원의 상세정보를 조회합니다')
