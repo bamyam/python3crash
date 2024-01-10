433587f7ae20b3a1f42228a58d0256fb4ece26b9
Seungjae, Seo <seoseungjae20209@gmail.com>
2024-01-10 14:10:12 +0900

    도서관리 프로그램 v1 - insert_book 구현

--- bamyam/BookDAO.py
+++ bamyam/BookDAO.py
@@ -1,11 +1,41 @@
+from bamyam.Book import Book
+import bamyam.dbinfo as dbinfo
+
+insertsql = 'insert into book(bkname, author, publisher, pubdate, retail, price, pctoff, mileage) ' \
+            ' values (%s,%s,%s,%s,%s,%s,%s,%s) '
+selectsql = 'select bkno, bkname, author, publisher, price from book'
+selectonesql = 'select * from book where bkno = %s'
+updatesql = 'update book set bkname = %s, author = %s, publisher = %s, pubdate = %s, '\
+            ' retail = %s, pctoff = %s '\
+            ' where bkno = %s'
+deletesql = 'delete from book where bkno = %s'
+
 class BookDAO:
     @staticmethod
-    def insert_book():
-        pass
+    def insert_book(bk):
+
+        cursor, conn = dbinfo.openConn()
+        params = [bk.bkname, bk.author, bk.publisher, bk.pubdate,
+                  bk.retail, bk.price, bk.pctoff, bk.mileage]
+        cursor.execute(insertsql, params)
+        conn.commit()
+        rowcnt = cursor.rowcount
+
+        dbinfo.closeConn(cursor, conn)
+
+        return rowcnt
 
     @staticmethod
     def select_book():
-        pass
+        sql = 'select bkname, author, publisher, pubdate, price from book ' \
+              ' order by sjno desc'
+
+        cursor, conn = dbinfo.openConn()
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        dbinfo.closeConn(cursor, conn)
+
+        return rows
 
     @staticmethod
     def selectone_book():
